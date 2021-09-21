8c2421d4836df3b4bd6e75ba49d22f0a85380d6f
ckcrider49@gmail.com <ckcritter>
2021-09-21 15:36:06 -0400

    now displays obs side by side with recon

--- autoencoder.py
+++ autoencoder.py
@@ -15,14 +15,14 @@ torch.set_default_dtype(torch.float64)
 plt.figure(figsize=(9, 2))
 plt.gray()
 plt.axis("off")
-
+_, sub = plt.subplots(2, 1);
 
 class Autoencoder(nn.Module):
     def __init__(self):
         super().__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 100, (3, 3), padding=1),
-            #nn.ReLU(),
+            nn.Conv2d(3, 10, (3, 3), padding=1),
+            nn.ReLU(),
             #nn.Conv2d(100, 40, (9, 9), stride=2, padding=1),
             #nn.ReLU(),
             #nn.Conv2d(40, 20, (1, 1), padding=1)
@@ -33,7 +33,7 @@ class Autoencoder(nn.Module):
             #nn.ReLU(),
             #nn.ConvTranspose2d(40, 100, (9, 9), stride=1, padding=1, output_padding=1),
             #nn.ReLU(),
-            nn.ConvTranspose2d(100, 3, (3, 3), padding=1),
+            nn.ConvTranspose2d(10, 3, (3, 3), padding=1),
             nn.Sigmoid()
         )
         
@@ -50,14 +50,15 @@ optimizer = torch.optim.Adam(model.parameters(),
                              weight_decay=1e-5)
 
 # Point to training loop video
-num_epochs = 200
+num_epochs = 1000
 outputs = []
 observation, reward, done, info = env.step(env.action_space.sample())
+f = lambda x: x / 255
 for epoch in range(num_epochs):
     #for i in range(0, 1000):
     observation, reward, done, info = env.step(env.action_space.sample())
     obs = observation.reshape(1, 3, 160, 210)
-    f = lambda x: x / 255
+    
     obs = f(obs)
     obs = torch.from_numpy( obs.astype('double') )
     recon = model(obs)
@@ -69,22 +70,28 @@ for epoch in range(num_epochs):
 
     print(f'Epoch:{epoch+1}, Loss:{loss.item():.4f}')
     outputs.append((epoch, obs, recon))
-
-for k in range(100, num_epochs, 10):
+    if done:
+        env.reset()
+        
+for k in range(1001, num_epochs, 100):    
     imgs = outputs[k][1].detach().numpy()
     recon = outputs[k][2].detach().numpy()
     for i, item in enumerate(imgs):
         if i >= 2: break
         Z = item.reshape(210, 160, 3)
-        im = plt.imshow(Z, interpolation='none', aspect='auto')
-        plt.colorbar(im, orientation='horizontal')
-        #plt.imshow(item)
-        plt.show()        
+        sub[0].imshow(Z, interpolation='none', aspect='auto')
 
     for i, item in enumerate(recon):
         if i >= 2: break
         Z = item.reshape(210, 160, 3)
-        im = plt.imshow(Z, interpolation='none', aspect='auto')
-        plt.colorbar(im, orientation='horizontal')
-        plt.show()
+        sub[1].imshow(Z, interpolation='none', aspect='auto')
+    plt.show()
 
+import cv2
+obs = f(cv2.imread("filename - Copy.png").reshape(1, 3, 160, 210))
+sub[0].imshow(obs.reshape(210, 160, 3), interpolation='none', aspect='auto')
+obs = torch.from_numpy( obs.astype('double') )
+recon = model(obs)
+sub[1].imshow(recon.detach().numpy().reshape(210, 160, 3), interpolation='none', aspect='auto')
+plt.show()
+print("hi")
